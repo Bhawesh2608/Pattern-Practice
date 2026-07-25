10ea0c84b4cdab31f9e474b507197ada1123bc12
Bhawesh Singh <bhaweshsinsinwar62@gmail.com>
2026-07-25 22:13:56 +0530

    "Add character Y star pattern"

--- Patterns_code/Print_X.c
+++ Patterns_code/Print_X.c
@@ -7,7 +7,7 @@ int main()
 	scanf("%d",&n);
 
 	for(int i=1; i<=n; i++) {
-		for(int j=1; j<=2*n-1; j++) {
+		for(int j=1; j<=n ; j++) {
 			if(j==i||j==(n+1-i)) {
 				printf("* ");
 			}
---
+++ Patterns_code/Print_Y.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+		printf("Enter N ");
+		scanf("%d",&n);
+
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=2*n-1; j++) {
+			if(j==i||j==(2*n-i)) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=n; j++) {
+			if( j==n) {
+				printf("* ");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
