86befd2edb8f715f010335f428210e008cc8a534
Bhawesh Singh <bhaweshsinsinwar62@gmail.com>
2026-08-06 20:52:16 +0530

    improve character A code

--- Patterns_code/Print_A.c
+++ Patterns_code/Print_A.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 int main() {
-	int n;
-	printf("Enter N ");
-	scanf("%d", &n);
-	for (int i = 1; i < n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if(j<=n/2) {
-				if (i==1||i==n/2||j==1||j==n/2) {
-					if (i==1 && (j==1 ||j==n/2)) {
-						printf("  ");
-					} else {
-						printf("* ");
-					}
-				} else {
-					printf("  ");
-				}
-			}
-		}
-		printf ("\n");
-	}
-	return 0;
+  int n;
+  printf("Enter N ");
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      if (i > 1 && (j == 1 || j == n)) {
+        printf("*");
+      } else if ((i == 1 && j > 1 && j < n) || i == n / 2 + 1) {
+        printf("*");
+      } else {
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
+  return 0;
 }
